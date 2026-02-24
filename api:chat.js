c859d12585119cd0a25675ed862d9a51a2db2ab1
jailbankmc <hansuelibs@gmail.com>
2026-02-24 19:46:52 +0100

    Update api:chat.js

--- api:chat.js
+++ api:chat.js
@@ -1,7 +1,7 @@
 import OpenAI from "openai";
 
 const client = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY
+  apiKey: process.env.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
 });
 
 export default async function handler(req, res){
